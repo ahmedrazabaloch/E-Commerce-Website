c416096e81dff655effeec7895534259dbc78a0b
Ahmed <ahmed.raza1054@gmail.com>
2023-08-26 02:12:25 +0500

    E-Commerce Sites
    E-Commerce Sites

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 Swal.fire({
   title:
-    "Almost completed <span>👍</span> with mobile responsivenes just footer section left & need some more cool effect will be added <hr/> There are some false value <hr/>function will be added when become JavaScript Cheta <span> 🔥 </span>",
+    "Almost completed <span>👍</span> with mobile responsivenes <span>📱</span> just the footer section is left and need some more cool effect need to be added.. <hr/> There are some false ❌ value present. <hr/>Functionality ⚙️ will be added when i become a <img style=width:30px src=https://cdn3.emoji.gg/emojis/3203-javascript.png> JavaScript Cheta <span>🔥</span>",
   showClass: {
     popup: "animate__animated animate__fadeInDown",
   },
