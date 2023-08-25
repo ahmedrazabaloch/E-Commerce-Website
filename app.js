be006f59c13495b0a5993c216c83da70c1e45b40
Ahmed <ahmed.raza1054@gmail.com>
2023-08-25 21:21:24 +0500

    ar cloth
    ar cloth

---
+++ app.js
@@ -0,0 +1,10 @@
+Swal.fire({
+  title:
+    "Almost completed <span>👍</span> with mobile responsivenes just footer section left & need some more cool effect will be added <hr/> There are some false value <hr/>function will be added when become JavaScript Cheta <span> 🔥 </span>",
+  showClass: {
+    popup: "animate__animated animate__fadeInDown",
+  },
+  hideClass: {
+    popup: "animate__animated animate__fadeOutUp",
+  },
+});
