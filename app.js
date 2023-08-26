06bf679b7da94be824ff5cb0be0272bfb421b197
Ahmed <ahmed.raza1054@gmail.com>
2023-08-27 04:02:26 +0500

    Mobile First & JS file update
    Mobile Responsiveness & JS file update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 Swal.fire({
   title:
-    "Almost completed <span>👍</span> with mobile responsivenes <span>📱</span> Need some more cool effect need to be added.. <hr/> There are some false ❌ value present. <hr/>Functionality ⚙️ will be added when i become a <img style=width:30px src=https://cdn3.emoji.gg/emojis/3203-javascript.png> JavaScript Cheta <span>🔥</span>",
+    "Almost completed<span>👍</span>with mobile responsivenes<span>📱</span><hr/>Need to be added some more cool effects<hr/> There are some false❌value present.<hr/>Functionality ⚙️ will be added when i become a <img style=width:30px src=https://cdn3.emoji.gg/emojis/3203-javascript.png> JavaScript Cheta<span>🔥</span>",
   showClass: {
     popup: "animate__animated animate__fadeInDown",
   },
