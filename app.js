646642cd047c8216e80c4fee8bd7423ebd01d2c6
Ahmed <ahmed.raza1054@gmail.com>
2023-08-28 03:13:50 +0500

    Alert update
    Alert update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 Swal.fire({
   title:
-    "Almost completed<span>👍</span>with mobile responsivenes<span>📱</span><hr/>Need to be added some more cool effects<hr/> There are some false❌value present.<hr/>Functionality ⚙️ will be added when i become a <img style=width:30px src=https://cdn3.emoji.gg/emojis/3203-javascript.png> JavaScript Cheta<span>🔥</span>",
+    "Almost completed<span>👍</span>with mobile responsivenes<span>📱</span><hr/>Need to be added some more cool effects<hr/> There are some false❌value present.<hr/>More Functionality ⚙️ will be added when i become a <img style=width:30px src=https://cdn3.emoji.gg/emojis/3203-javascript.png> JavaScript Cheta<span>🔥</span><hr/>Figma Design File Link <a href=https://www.figma.com/file/InGx8EtZdT60ZvS2bOZe6Z/E-commerce-Website-Template-(Freebie)-(Community)?type=design&node-id=20-2&mode=design target=blank><br/>👇<br/><u>Click Here</u></a>",
   showClass: {
     popup: "animate__animated animate__fadeInDown",
   },
@@ -9,8 +9,6 @@ Swal.fire({
   },
 });
 
-
-function showAlert(){
-  alert("Page coming soon\nUnder Developent")
-  
+function showAlert() {
+  alert("Page coming soon\nUnder Developent");
 }
